95fb3f5c0977b9174dc360e47c8ec13d9b94dd58
NichHarris <harris.nicholas1998@gmail.com>
2020-11-14 23:57:00 -0500

    Remove dox

--- send_sms.py
+++ send_sms.py
@@ -1,16 +1,17 @@
-import twilio
+import os
 from twilio.rest import Client
 
-# Your Account SID from twilio.com/console
-account_sid = "AC0f3243186875bd7d009d16229b31cea3"
-# Your Auth Token from twilio.com/console
-auth_token  = "c48189e21f517f9ec92d4201a557d85c"
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+myNum = os.environ['MY_NUM']
+twilioNum = os.environ['TWILIO_NUMBER']
 
 client = Client(account_sid, auth_token)
 link = "X"
+
 message = client.messages.create(
-    to="+14388327376", 
-    from_="+12184927786",
+    to=myNum, 
+    from_=twilioNum,
     body="RYZEN 5600X LINK: " + link)
 
 print(message.sid)
