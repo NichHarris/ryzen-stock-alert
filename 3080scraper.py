f01bb2b089c90dee1cb25108e2c2821e0837f423
NichHarris <harris.nicholas1998@gmail.com>
2020-11-17 01:02:39 -0500

    Add BeautifulSoup4 functionality to search through product catalogues and return each link to products which meet keyword requirements - Add seperate file for 3080 sites and for 3080scraper.py

--- 3080scraper.py
+++ 3080scraper.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from enum import Enum
 import requests
 import smtplib, ssl
+from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 from twilio.rest import Client
 
@@ -27,6 +28,9 @@ port = 587
 with open('sites3080.json', 'r') as f:
     sites = json.load(f)
 
+
+
+
 # Twilio
 print("Initializing Twilio...")
 client = Client(account_sid, auth_token)
@@ -44,18 +48,18 @@ def timeFunction():
     currentTime = now.strftime("%H:%M:%S")
     return currentTime
 
-def alert(site, currentTime):
+def alert(site, link, currentTime):
     open('alerts.txt', 'w').close()
     product = site.get('name')
     print("{} IN STOCK".format(product))
-    print(site.get('url'))
+    print(link)
     if openWebBrowser:
-        webbrowser.open(site.get('url'), new=1)
-    send_text(site.get('url'))
-    disc_webhook(product, site.get('url'))
+        webbrowser.open(link, new=1)
+    send_text(link)
+    disc_webhook(product,link)
     #send_email(site.get('url'))
     with open('alerts.txt', 'w') as f:
-        f.write("\n{} IN STOCK: {} \n Time: {}".format(product, site.get('url'),currentTime))
+        f.write("\n{} IN STOCK: {} \n Time: {}".format(product, link,currentTime))
     # time.sleep(60)
 
 def send_email(url):
@@ -101,23 +105,34 @@ def main():
             currentTime = timeFunction()
             if site.get('enabled'):
                 print("\tChecking {} ...".format(site.get('name')))
+                ## Getting link to 3080 product page
                 try:
                     html = urllib_get(site.get('url'))
                 except Exception as e:
-                    print("\tConnection Failed...")
-                    print("\tSkipping")
-                    continue
+                        print("\tConnection Failed...")
+                        print("\tSkipping")
+                        continue
                 
+                soup = BeautifulSoup(html, 'html.parser')
                 keyword = site.get('Keyword')
                 isAlert = site.get('alert')
                 index = html.upper().find(keyword.upper())
-
+                ## TODO: Use the tags to individually search each page
                 if isAlert and index != -1:
-                    alert(site, currentTime)
+                    ## Using BS4 to find all the links to products on this page
+                    for link in soup.find_all("a", class_="item-image", href=True):
+                        tags = link['href']
+                        print("I worked: ")
+                        print(tags)
+                        # alert(site, tags, currentTime)
                 elif not isAlert and index == -1:
-                    alert(site, currentTime)
-                print(currentTime)
-            time.sleep(10)          
+                    ## Using BS4 to find all the links to products on this page
+                    for link in soup.find_all("a", class_="product-image", href=True):
+                        tags = link['href']
+                        # alert(site, tags, currentTime)
+                    print(currentTime)
+                time.sleep(10)          
         time.sleep(30)
+
 if __name__ == '__main__':
     main()
